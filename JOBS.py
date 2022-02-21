02f260f1cdab23de5a5340851085da6b653f527f
Timur G <94556970+Zabava1@users.noreply.github.com>
2022-02-21 21:55:38 +0300

    Тест создание общего файла списка работ
    блаблабла

---
+++ JOBS.py
@@ -0,0 +1,21 @@
+
+stajorsber1=  ['sql', 'python', 'random forest', 'xgboost', 'Парсинг данных']
+dataScientist_DataAnalystsber2= ['sql', 'python', 'pandas', 'pyspark', 'matplotlib', 'numpy', 'sklearn', 'scipy', 'Oracle', 'Teradata', 'MS SQL', 'владение инструментами математической статистики', 'основы Machine Learning']
+dataScientistlibSber3= ['sql', 'python', 'ML', 'Reinforcement Learning', 'NLP', 'Time Series', 'AutoML', 'DML', 'SSL', 'Git', 'Bitbucket', 'Docker', 'pyspark', 'rest api']
+
+
+dataScientist_DataAnalystNBI= [['sql', 'python', 'sklearn', 'scipy', 'numpy', 'pandas'], ['Keras', 'PyTorch', 'Tensorflow', 'sklearn'], ['Docker', 'python']]  #2 из 3х напр
+dataScientistDomrfbank= ['sql', 'python', 'владение инструментами математической статистики', 'Upper-Intermediate']
+dataScientistAdv= ['python', 'Gretl', 'R', 'презентации', 'dashboards', 'владение инструментами математической статистики', 'Upper-Intermediate']
+
+
+dataScientistVTB= ['sql', 'python', 'владение инструментами математической статистики', 'MS Office', 'эконометрика', 'pandas', 'numpy', 'sklearn', 'Hadoop/Spark']
+dataScientistYoulaVK= ['sql', 'python', 'Excel', 'владение инструментами математической статистики', ['Tableau', 'Power BI']] #работа с любой BI-системой
+dataScientistSAS= ['sql', 'python', 'владение инструментами математической статистики']
+dataScientistMcdonalds= ['sql', 'python', 'kaggle', 'pet projects', 'владение инструментами математической статистики']
+
+
+dataAnalystsSchlumberger= ['sql', 'python', 'ML', 'владение инструментами математической статистики', '...', 'Intermediate']
+dataScientistZaymigo= ['sql', 'python', 'владение инструментами математической статистик', 'sklearn', 'numpy', 'pandas', 'matplotlib', 'R', 'SPSS', 'Statistica', 'Stata']
+dataScientistAlgoMost= ['sql', 'python', 'sklearn', 'numpy', 'pandas', 'xgboost', 'lightgbm', 'catboost', 'PyTorch', 'Tensorflow', 'OpenCV', 'git', 'Hadoop', 'pyspark', 'Kaggle', 'Hive', 'techEng']
+dataScientistInterfucks= ['sql', 'python', 'sklearn', 'numpy', 'pandas', 'владение инструментами математической статистики', 'ML', 'git']
